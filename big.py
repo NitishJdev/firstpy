b6595016bee7a5c30fcb3a46ed3c513dc1a5cc65
Nitish Joshi <nitishdev86@gmail.com>
2019-12-05 11:41:09 +0530

    Update big.py

--- big.py
+++ big.py
@@ -2,10 +2,10 @@ big=0
 small=0
 print('before',big)
 for thing in [-25,-99,0,9,12,13,54,6,78,98]:
-    if thing>big:
+    if thing>big:#find biggest number from thing
         big=thing
     print(big,thing)
-    if thing<small:
+    if thing<small:#find smallest number from thing
         small=thing
     print(small,thing)
 print('after',big)
