cc4e74a697a9c5dcd305677919a3ce3da9ae00ae
Nitish Joshi <nitishdev86@gmail.com>
2019-12-05 11:53:15 +0530

    Update big.py

--- big.py
+++ big.py
@@ -1,12 +1,16 @@
 big=0
 small=0
+fork=0
 print('before',big)
 for thing in [-25,-99,0,9,12,13,54,6,78,98]:
-    if thing>big:#find biggest number from thing
+    fork=fork+1
+    print(fork,thing)
+    if thing>big:
         big=thing
     print(big,thing)
-    if thing<small:#find smallest number from thing
+    if thing<small:
         small=thing
     print(small,thing)
 print('after',big)
 print('after',small)
+print('after',fork)
