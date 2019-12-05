9780366098eceadeed814b2253c1f741bc5a18e7
Nitish Joshi <nitishdev86@gmail.com>
2019-12-05 11:54:46 +0530

    Update big.py

--- big.py
+++ big.py
@@ -3,12 +3,12 @@ small=0
 fork=0
 print('before',big)
 for thing in [-25,-99,0,9,12,13,54,6,78,98]:
-    fork=fork+1
+    fork=fork+1 #calculates executed loops
     print(fork,thing)
-    if thing>big:
+    if thing>big: #finds biggest number from thing
         big=thing
     print(big,thing)
-    if thing<small:
+    if thing<small: #finds smallest number from thing
         small=thing
     print(small,thing)
 print('after',big)
